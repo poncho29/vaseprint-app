c0a3811bdbc481598412e7427bbc1238c2beae3a
poncho29 <meneses0828@gmail.com>
2023-01-18 21:41:31 -0500

    view admin user in progress

--- src/views/admin/AdminUser.jsx
+++ src/views/admin/AdminUser.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const AdminUser = () => {
   return (
-    <div>AdminUser</div>
+    <div className="user__container">
+      <div className="user__filters">
+        <h2>Filtros</h2>
+      </div>
+
+      <table style={{ width: '100%', border: '1px solid #333' }}>
+        <thead>
+          <tr>
+            <th style={{ border: '1px solid #333' }}>ID</th>
+            <th style={{ border: '1px solid #333' }}>Email</th>
+            <th style={{ border: '1px solid #333' }}>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: '1px solid #333' }}>1</td>
+            <td style={{ border: '1px solid #333' }}>Sebas@gmail.com</td>
+            <td style={{ border: '1px solid #333' }}>Admin</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
